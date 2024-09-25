96913631acfc0199df418ac53777b7648e205f22
akotoy <148432161+anthonykotoyan@users.noreply.github.com>
2024-09-25 14:31:00 -0700

    Game loop Main.java

--- Main.java
+++ Main.java
@@ -2,28 +2,38 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class Main {
+    private static int money;
     public static void main(String[] args) {
         // Create the main frame
         JFrame frame = new JFrame("Custom Button Example");
-        frame.setSize(400, 400);
+        int width = 1200;
+        int height = 800;
+
+        frame.setSize(width, height);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         // Create a panel with null layout for absolute positioning
         JPanel panel = new JPanel();
-        panel.setLayout(null);
 
         // Add custom buttons with specific positions
-        Upgrade button1 = new Upgrade("Nash", 5, 50, 50, 100, 0, "Our Beautiful Principle", "nash.jpg");
-        Upgrade button2 = new Upgrade("Empty Upgrade", 200, 100, 100, 40, 0, "");
-        Upgrade button3 = new Upgrade();
+        ExtraClickUpgrade nashUpgrade = new ExtraClickUpgrade(panel, 1,"Nash", 0, 0, 100, 100, 10, "Our Beautiful Principle", "nash.jpg");
+        Clicker guillaumeClicker = new Clicker(panel, width/2 - 50, height/2 - 50, 100,100, "guilluame.jpg");
 
-        panel.add(button1);
-        panel.add(button2);
-        panel.add(button3);
+
+
 
         frame.add(panel);
         frame.setVisible(true);
 
-        System.out.println(button1);
+
+    }
+    public static void setMoney(int numClicks){
+
+        money = numClicks;
+        System.out.println(money);
+    }
+    public static int getMoney(){
+
+        return money;
     }
 }
