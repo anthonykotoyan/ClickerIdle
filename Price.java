84934f8eac98428c3c561794308457f43c7bc38d
Evan O'Keefe <73354669+evan-okeefe@users.noreply.github.com>
2024-09-26 09:23:06 -0700

    Price Class

---
+++ Price.java
@@ -0,0 +1,35 @@
+public class Price {
+    private int price;
+    private String type;
+    private int modifier;
+
+    public int getPrice() {
+        return price;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public int getModifier() {
+        return modifier;
+    }
+
+    public Price(int price, String type, int modifier) {
+        this.price = price;
+        this.type = type;
+        this.modifier = modifier;
+    }
+
+    public void updatePrice(){
+        if (type == "+"){
+            price += modifier;
+        }
+        else if (type == "*"){
+            price *= modifier;
+        }
+        else if (type == "^") {
+            price = (int) Math.pow(price, modifier);
+        }
+    }
+}
